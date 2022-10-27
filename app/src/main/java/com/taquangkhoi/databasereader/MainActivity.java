b140448008d76fd22e5f0b095c6f08600744540c
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-27 20:31:46 +0700

    Viết GetRecentDatabase, CreateOneRecentRecord, UpdateOneRecentRecord

--- app/src/main/java/com/taquangkhoi/databasereader/MainActivity.java
+++ app/src/main/java/com/taquangkhoi/databasereader/MainActivity.java
@@ -7,9 +7,9 @@ import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnNewDatabase;
-    Button btnOpenDatabase;
+    Button btnNewDatabase, btnOpenDatabase;
     ListView lvwRecentDatabase;
+    RecentDBHandler recentDBHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         btnNewDatabase = findViewById(R.id.btn_new_database);
         btnOpenDatabase = findViewById(R.id.btn_open_database);
         lvwRecentDatabase = findViewById(R.id.lvw_recent_database);
+        recentDBHandler = new RecentDBHandler(this);
     }
 
     private void AddEvents() {
@@ -36,7 +37,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Read SQLite Database to get recent database list
-    private void GetRecentDatabase() {
-        
+    private boolean GetRecentDatabase() {
+        // Get recent database list
+        return true;
+    }
+
+    // Create new SQLite database to store recent database list
+    private void CreateOneRecentRecord(String path, String openTime) {
+        recentDBHandler.addNewRecent(path, openTime);
+    }
+
+    private void UpdateOneRecentRecord(String path, String openTime) {
+//        recentDBHandler.updateRecent(path, openTime);
     }
 }
