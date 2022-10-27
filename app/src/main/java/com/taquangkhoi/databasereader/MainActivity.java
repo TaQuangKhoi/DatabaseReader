c74bacfbbdba54ac9ebc3b3b4bd29c17c34a9197
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-27 19:23:34 +0700

    AddControls and AddEvents

--- app/src/main/java/com/taquangkhoi/databasereader/MainActivity.java
+++ app/src/main/java/com/taquangkhoi/databasereader/MainActivity.java
@@ -3,12 +3,40 @@ package com.taquangkhoi.databasereader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnNewDatabase;
+    Button btnOpenDatabase;
+    ListView lvwRecentDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AddControls();
+        AddEvents();
+    }
+
+    private void AddControls() {
+        btnNewDatabase = findViewById(R.id.btn_new_database);
+        btnOpenDatabase = findViewById(R.id.btn_open_database);
+        lvwRecentDatabase = findViewById(R.id.lvw_recent_database);
+    }
+
+    private void AddEvents() {
+        btnNewDatabase.setOnClickListener(v -> {
+
+        });
+        btnOpenDatabase.setOnClickListener(v -> {
+
+        });
+    }
+
+    // Read SQLite Database to get recent database list
+    private void GetRecentDatabase() {
+        
     }
 }
